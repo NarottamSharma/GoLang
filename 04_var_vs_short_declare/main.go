f8a09e736405e13ec82e0b39510ccad017738fbe
Narottam Sharma <narottamphodegaa@gmail.com>
2026-01-21 10:13:38 +0530

    add main.go with string manipulation and formatting

---
+++ 04_var_vs_short_declare/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var city string
+	city = "London"
+	fmt.Println(city)
+	country := "India"
+	fmt.Println(country)
+
+	fName , lname := "Narottam", "Sharma"
+	fmt.Println("My full name is",fName+" "+lname)
+}
---
+++ 05_basic_type_string/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"fmt"
+	"strings"
+)
+
+func main(){
+	fName := "Narottam"
+	lName := "Sharma"
+	fullName := fName+" "+lName
+	fmt.Println(fullName)
+	a:= strings.ToUpper("narottam")
+	fmt.Println(a)
+}
